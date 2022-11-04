cdf80fb2a5cf8bc5732d33572f06daaec2455b14
LucasAlvesG <117421570+LucasAlvesG@users.noreply.github.com>
2022-11-04 11:53:58 -0300

    Update exercicio1.js

--- exercicio1.js
+++ exercicio1.js
@@ -1,11 +1,10 @@
-
 let usuario = prompt("digite o número do mês")
-if(usuario<1 || usuario>12){
-    console.log("mês invalido")
+if(usuario<1 || usuario>12){ //não sei por que mas quando eu coloco 0 ou 1 no prompt o comando identifica como janeiro... 
+    console.log("mês invalido") //... e 2 fevereiro e assim por diante ate o mes 12 que ele identifica como dezembro.
 }
 else{
 function diasNoMes(mes) {
-    let data = new Date(0, mes, 0);
+    let data = new Date(0 , mes, 0);
     return data.getDate();
 }
 console.log(diasNoMes(usuario))
